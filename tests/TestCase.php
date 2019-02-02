0ca435262bd5d8b44cab96b7476357e596f23634
Samuel Štancl <samuel.stancl@gmail.com>
2019-02-02 12:28:21 +0100

    Try to fix Call to undef. method Redis::connect()

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Stancl\Tenancy\Tests;
 
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Redis as LaravelRedis; // https://travis-ci.com/stancl/tenancy/jobs/174861107#L615
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -15,7 +15,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::setUp();
 
-        Redis::connection('tenancy')->flushdb();
+        LaravelRedis::connection('tenancy')->flushdb();
 
         tenant()->create('phpunit.localhost');
 
