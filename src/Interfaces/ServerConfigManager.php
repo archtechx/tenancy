256e88444ca0027611bdddae99360374bef3bc82
Samuel Štancl <samuel.stancl@gmail.com>
2019-02-07 15:22:57 +0100

    Remove unfinished features, expand README

--- src/Interfaces/ServerConfigManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stancl\Tenancy\Interfaces;
-
-interface ServerConfigManager
-{
-    public function addVhost(string $domain, string $file): bool;
-    public function deployCertificate(string $domain): bool;
-}
--- src/config/tenancy.php
+++ src/config/tenancy.php
@@ -30,35 +30,4 @@ return [
             // 's3',
         ],
     ],
-    'server' => [
-        'manager' => 'Stancl\Tenancy\ServerConfigManagers\NginxConfigManager',
-        'file' => [
-            'single' => true, // single file for all tenant vhosts
-            'path' => '/etc/nginx/sites-available/tenants.conf',
-            /*
-            'single' => false,
-            'path' => [
-                'prefix' => '/etc/nginx/sites-available/tenants/tenant',
-                'suffix' => '.conf',
-                // results in: '/etc/nginx/sites-available/tenants/tenant' . $uuid . '.conf'
-            ]
-            */
-        ],
-        'nginx' => [
-            'webroot' => '/var/www/html',
-            'vhost' => "
-            server {
-                include includes/tenancy;
-                server_name %host%;
-            }",
-            'extra_certbot_args' => [
-                '--must-staple',
-                '--config-dir', '/etc/letsencrypt_tenancy',
-                '--work-dir', '/var/lib/letsencrypt_tenancy',
-                '--logs-dir', '/var/log/letsencrypt_tenancy',
-                '--staging', // obtains a fake cert intended for testing certbot
-                // '--email', 'your@email', // if you haven't created an account in certbot yet
-            ],
-        ],
-    ]
 ];
