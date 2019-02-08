9ec4fd931a1db264538a7886097e4d3813575bc9
Samuel Štancl <samuel.stancl@gmail.com>
2019-02-08 23:25:50 +0100

    First TenantAssetTest

--- tests/TenantAssetTest.php
+++ tests/TenantAssetTest.php
@@ -7,9 +7,19 @@ class TenantAssetTest extends TestCase
     /** @test */
     public function asset_can_be_accessed_using_the_url_returned_by_the_tenant_asset_helper()
     {
-        $this->markTestIncomplete();
         $filename = 'testfile' . $this->randomString(10);
         \Storage::disk('public')->put($filename, 'bar');
-        $this->get(tenant_asset($filename))->assertSee('bar');
+        $path = storage_path("app/public/$filename");
+        
+        // response()->file() returns BinaryFileResponse whose content is
+        // inaccessible via getContent, so ->assertSee() can't be used
+        $this->get(tenant_asset($filename))->assertSuccessful();
+        $this->assertFileExists($path);
+
+        $f = fopen($path, 'r');
+        $content = fread($f, filesize($path));
+        fclose($f);
+
+        $this->assertSame('bar', $content);
     }
 }
