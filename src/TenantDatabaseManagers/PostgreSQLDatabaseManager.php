5e45c310a4f67eb0d649e472d5af138154100cc3
Samuel Štancl <samuel.stancl@gmail.com>
2019-06-29 14:16:05 +0200

    Remove single quotes

--- src/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
+++ src/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
@@ -9,11 +9,11 @@ class PostgreSQLDatabaseManager implements TenantDatabaseManager
 {
     public function createDatabase(string $name): bool
     {
-        return DB::statement("CREATE DATABASE '$name' WITH ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'");
+        return DB::statement("CREATE DATABASE $name WITH ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'");
     }
 
     public function deleteDatabase(string $name): bool
     {
-        return DB::statement("DROP DATABASE '$name'");
+        return DB::statement("DROP DATABASE $name");
     }
 }
