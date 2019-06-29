657e9784a5d588ea02d90ddee5f295d9c1f34230
Samuel Štancl <samuel.stancl@gmail.com>
2019-06-29 13:46:18 +0200

    Add PGSQL username to test env setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -67,6 +67,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
                 'suffix' => '.sqlite',
             ],
             'database.connections.sqlite.database' => ':memory:',
+            'database.connections.pgsql.username' => 'postgres',
             'tenancy.filesystem.disks' => [
                 'local',
                 'public',
