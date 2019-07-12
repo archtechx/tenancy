4e26118facae69ae8c37b6ca3481f44b9a44b1f2
Samuel Štancl <samuel.stancl@gmail.com>
2019-07-12 23:01:01 +0200

    Fix bugs

--- src/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
+++ src/TenantDatabaseManagers/PostgreSQLDatabaseManager.php
@@ -9,7 +9,7 @@ class PostgreSQLDatabaseManager implements TenantDatabaseManager
 {
     public function createDatabase(string $name): bool
     {
-        return DB::statement("CREATE DATABASE $name WITH ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'");
+        return DB::statement("CREATE DATABASE $name WITH TEMPLATE=template0");
     }
 
     public function deleteDatabase(string $name): bool
--- tests/TenantDatabaseManagerTest.php
+++ tests/TenantDatabaseManagerTest.php
@@ -87,7 +87,7 @@ class TenantDatabaseManagerTest extends TestCase
 
         config()->set('database.default', 'pgsql');
 
-        $db_name = 'testdatabase' . $this->randomString(10);
+        $db_name = strtolower('testdatabase' . $this->randomString(10));
         $this->assertTrue(app(DatabaseManager::class)->create($db_name, 'pgsql'));
         $this->assertNotEmpty(DB::select("SELECT datname FROM pg_database WHERE datname = '$db_name'"));
 
@@ -104,7 +104,7 @@ class TenantDatabaseManagerTest extends TestCase
 
         config()->set('database.default', 'pgsql');
 
-        $db_name = 'testdatabase' . $this->randomString(10);
+        $db_name = strtolower('testdatabase' . $this->randomString(10));
 
         $databaseManagers = config('tenancy.database_managers');
         $job = new QueuedTenantDatabaseCreator(app($databaseManagers['pgsql']), $db_name);
