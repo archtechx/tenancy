01007c3be70a30b35c9cebc8324a68d6479765e8
Samuel Štancl <samuel.stancl@gmail.com>
2019-08-09 22:05:43 +0200

    wip

--- src/Tenant.php
+++ src/Tenant.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tenant extends Model
 {
-    // todo if not attribute exists in db, put into json data
+    public function getDataColumn()
+    {
+        return 'data';
+    }
+
+    public function put(string $key, $value)
+    {
+        if ($this->getConnection()->getSchemaBuilder()->hasColumn($this->getTable(), $key)) {
+            $this->update([$key => $value]);
+        } else {
+            $obj = json_decode($this->{$this->getDataColumn()});
+            $obj->$key = $value;
+
+            $this->update([$this->getDataColumn() => json_encode($obj)]);
+        }
+    }
 }
