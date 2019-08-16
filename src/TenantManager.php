89c7289b66f29fc8bbc63457133896f372d380a4
Samuel Štancl <samuel.stancl@gmail.com>
2019-08-16 18:26:18 +0200

    Switch order of conditions

--- src/TenantManager.php
+++ src/TenantManager.php
@@ -274,7 +274,7 @@ final class TenantManager
         $uuid = $uuid ?: $this->tenant['uuid'];
 
         if (array_key_exists('uuid', $this->tenant) && $uuid === $this->tenant['uuid'] &&
-            array_key_exists($key, $this->tenant) && ! is_array($key)) {
+            ! is_array($key) && array_key_exists($key, $this->tenant)) {
             return $this->tenant[$key];
         }
 
