965f67c747d907fa449180df06748a51444d5d19
Samuel Štancl <samuel.stancl@gmail.com>
2019-08-23 12:58:29 +0200

    Apply middleware to $middleware instead of web, fix tests

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -286,6 +286,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected \$middlewarePriority = [
+        \Stancl\Tenancy\Middleware\PreventAccessFromTenantDomains::class,
         \Stancl\Tenancy\Middleware\InitializeTenancy::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
