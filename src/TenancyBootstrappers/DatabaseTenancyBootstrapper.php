f471a1a7e452dbb73ea4e5ce341e9b5255f8674b
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-05 19:25:47 +0200

    TenancyBootstrappers WIP

---
+++ src/TenancyBootstrappers/DatabaseTenancyBootstrapper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Stancl\Tenancy\TenancyBoostrappers;
+
+use Illuminate\Foundation\Application;
+use Stancl\Tenancy\Contracts\TenancyBootstrapper;
+use Stancl\Tenancy\DatabaseManager;
+
+class DatabaseTenancyBootstrapper implements TenancyBootstrapper
+{
+    /** @var Application */
+    protected $app;
+
+    /** @var DatabaseManager */
+    protected $database;
+
+    public function __construct(Application $app, DatabaseManager $database)
+    {
+        $this->app = $app;
+        $this->database = $database;
+    }
+
+    public function start(Tenant $tenant)
+    {
+        $this->database->connect($tenant->getDatabaseName());
+    }
+
+    public function end()
+    {
+        $this->database->reconnect();
+    }
+}
