798df54d761a15d469b02fbd845095eb3dc7b3e3
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-05 19:26:19 +0200

    HasArrayAccess trait

---
+++ src/Traits/HasArrayAccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Stancl\Tenancy\Traits;
+
+trait HasArrayAccess
+{
+    public function offsetExists($offset): bool
+    {
+        return property_exists($this, $offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->$offset = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->$offset);
+    }
+}
