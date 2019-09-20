9892c3afc753fab7d4960e2de95de8b6dd8e3e10
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-20 17:56:22 +0200

    Data cache test

---
+++ tests/TenantClassTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Stancl\Tenancy\Tests;
+
+use Mockery;
+use Stancl\Tenancy\Contracts\StorageDriver;
+use Stancl\Tenancy\Tenant;
+
+class TenantClassTest extends TestCase
+{
+    public $autoInitTenancy = false;
+    public $autoCreateTenant = false;
+
+    /** @test */
+    public function data_cache_works_properly()
+    { 
+        $spy = Mockery::spy(config('tenancy.storage_driver'))->makePartial();
+        $this->instance(StorageDriver::class, $spy);
+
+        $tenant = Tenant::create(['foo.localhost'], ['foo' => 'bar']);
+        $this->assertSame('bar', $tenant->data['foo']);
+
+        $tenant->put('abc', 'xyz');
+        $this->assertSame('xyz', $tenant->data['abc']);
+
+        $tenant->put(['aaa' => 'bbb', 'ccc' => 'ddd']);
+        $this->assertSame('bbb', $tenant->data['aaa']);
+        $this->assertSame('ddd', $tenant->data['ccc']);
+
+        $spy->shouldNotHaveReceived('get');
+        Mockery::close();
+    }
+}
