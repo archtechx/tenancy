bd1c829520c8e83ccbca727857fb24d047060c0b
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-23 11:54:38 +0200

    TenantCouldNotBeIdentified ProvidesSolution

--- src/Exceptions/TenantCouldNotBeIdentifiedException.php
+++ src/Exceptions/TenantCouldNotBeIdentifiedException.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\Exceptions;
 
-class TenantCouldNotBeIdentifiedException extends \Exception
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
+
+class TenantCouldNotBeIdentifiedException extends \Exception implements ProvidesSolution
 {
     public function __construct($domain)
     {
         $this->message = "Tenant could not be identified on domain $domain";
+    }
+
+    public function getSolution(): Solution
+    {
+        return BaseSolution::create('Tenant could not be identified on this domain')
+            ->setSolutionDescription('Did you forget to create a tenant for this domain?')
+            ->setDocumentationLinks([
+                'Creating Tenants' => 'https://tenancy.samuelstancl.me/docs/v2/creating-tenants/',
+            ]);
     }
 }
