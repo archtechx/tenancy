935948fd7e1af7b6456386daa3cebfcde7e1e703
Samuel Štancl <samuel.stancl@gmail.com>
2019-09-30 18:04:35 +0200

    Replace webpatser/laravel-uuid with ramsey/uuid

--- src/UniqueIDGenerators/UUIDGenerator.php
+++ src/UniqueIDGenerators/UUIDGenerator.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\UniqueIDGenerators;
 
+use Ramsey\Uuid\Uuid;
 use Stancl\Tenancy\Contracts\UniqueIdentifierGenerator;
 
 class UUIDGenerator implements UniqueIdentifierGenerator
 {
     public static function generate(array $domains, array $data = []): string
     {
-        return (string) \Webpatser\Uuid\Uuid::generate(1, $domains[0] ?? '');
+        return Uuid::uuid4()->toString();
     }
 }
