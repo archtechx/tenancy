bde103560977cd399553c7dc862f838ef619dadb
stancl <samuel.stancl@gmail.com>
2019-09-30 16:42:01 +0000

    Apply fixes from StyleCI

--- src/StorageDrivers/Database/CentralConnection.php
+++ src/StorageDrivers/Database/CentralConnection.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\StorageDrivers\Database;
 
-use Stancl\Tenancy\DatabaseManager;
-
 trait CentralConnection
 {
     public function getConnectionName()
