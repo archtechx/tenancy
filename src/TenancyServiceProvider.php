463c566edafe19abb9a985720b8bd6329b2f93ef
Samuel Štancl <samuel.stancl@gmail.com>
2019-10-04 19:54:24 +0200

    Create command test

--- src/TenancyServiceProvider.php
+++ src/TenancyServiceProvider.php
@@ -65,6 +65,7 @@ class TenancyServiceProvider extends ServiceProvider
             Commands\Migrate::class,
             Commands\Rollback::class,
             Commands\TenantList::class,
+            Commands\CreateTenant::class,
             Commands\MigrateFresh::class,
         ]);
 
