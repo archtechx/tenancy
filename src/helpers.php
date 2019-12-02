a8f84d280860e003ca00a8e67ce78ec8962bbd24
Samuel Štancl <samuel.stancl@gmail.com>
2019-12-02 17:57:16 +0100

    Accept closures in tenant() helper

--- src/helpers.php
+++ src/helpers.php
@@ -18,14 +18,23 @@ if (! function_exists('tenancy')) {
 }
 
 if (! function_exists('tenant')) {
-    /** @return Tenant|mixed */
+    /**
+     * Get a key from the current tenant's storage.
+     *
+     * @param string|Closure|null $key
+     * @return Tenant|mixed
+     */
     function tenant($key = null)
     {
-        if (! is_null($key)) {
-            return optional(app(Tenant::class))->get($key) ?? null;
+        if (is_null($key)) {
+            return app(Tenant::class);
         }
 
-        return app(Tenant::class);
+        if ($key instanceof Closure) {
+            return app(Tenant::class)->run($key);
+        }
+
+        return optional(app(Tenant::class))->get($key) ?? null;
     }
 }
 
