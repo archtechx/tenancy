ba8e9b0261b03904fb5ce22c3f1aa35797e2ef58
Samuel Štancl <samuel.stancl@gmail.com>
2020-03-04 21:13:51 +0100

    verify both laravel versions are used

--- tests/TenantManagerTest.php
+++ tests/TenantManagerTest.php
@@ -25,6 +25,10 @@ class TenantManagerTest extends TestCase
     /** @test */
     public function current_tenant_can_be_retrieved_using_getTenant()
     {
+        if (app()->version()[0] === '6') {
+            $this->fail('laravel 6 is used');
+        }
+
         $tenant = Tenant::new()->withDomains(['test2.localhost'])->save();
 
         tenancy()->init('test2.localhost');
@@ -35,6 +39,10 @@ class TenantManagerTest extends TestCase
     /** @test */
     public function initById_works()
     {
+        if (app()->version()[0] === '6') {
+            $this->fail('laravel 7 is used');
+        }
+
         $tenant = Tenant::new()->withDomains(['foo.localhost'])->save();
 
         $this->assertNotEquals($tenant, tenancy()->getTenant());
