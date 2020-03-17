0540fe4581fc98f0ad28f5ebf727c9946d9353dc
Samuel Štancl <samuel.stancl@gmail.com>
2020-03-17 16:41:38 +0100

    Fix indentation

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -78,8 +78,8 @@ class Install extends Command
             return str_replace(
                 'protected $middlewarePriority = [',
                 "protected \$middlewarePriority = [
-            \Stancl\Tenancy\Middleware\PreventAccessFromTenantDomains::class,
-            \Stancl\Tenancy\Middleware\InitializeTenancy::class,",
+        \Stancl\Tenancy\Middleware\PreventAccessFromTenantDomains::class,
+        \Stancl\Tenancy\Middleware\InitializeTenancy::class,",
                 file_get_contents(app_path('Http/Kernel.php'))
             );
         } else {
