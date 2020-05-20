2fedd5ce88e8eac4c4277e058b7f773992896d87
Samuel Štancl <samuel.stancl@gmail.com>
2020-05-20 21:50:10 +0200

    Tenant-specific maintenance mode

---
+++ src/Middleware/CheckTenantForMaintenanceMode.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Stancl\Tenancy\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Http\Request;
+use Stancl\Tenancy\Exceptions\TenancyNotInitializedException;
+use Symfony\Component\HttpFoundation\IpUtils;
+
+class CheckTenantForMaintenanceMode extends CheckForMaintenanceMode
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (! tenant()) {
+            throw new TenancyNotInitializedException;
+        }
+
+        if (tenant('maintenance_mode')) {
+            $data = tenant('maintenance_mode');
+
+            if (isset($data['allowed']) && IpUtils::checkIp($request->ip(), (array) $data['allowed'])) {
+                return $next($request);
+            }
+
+            if ($this->inExceptArray($request)) {
+                return $next($request);
+            }
+
+            throw new MaintenanceModeException($data['time'], $data['retry'], $data['message']);
+        }
+
+        return $next($request);
+    }
+}
---
+++ tests/MaintenanceModeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Stancl\Tenancy\Tests;
+
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Database\Concerns\MaintenanceMode;
+use Stancl\Tenancy\Middleware\CheckTenantForMaintenanceMode;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Tests\Etc\Tenant;
+
+class MaintenanceModeTest extends TestCase
+{
+    /** @test */
+    public function tenant_can_be_in_maintenance_mode()
+    {
+        Route::get('/foo', function () {
+            return 'bar';
+        })->middleware([InitializeTenancyByDomain::class, CheckTenantForMaintenanceMode::class]);
+        
+        $tenant = MaintenanceTenant::create();
+        $tenant->domains()->create([
+            'domain' => 'acme.localhost',
+        ]);
+
+        $this->get('http://acme.localhost/foo')
+            ->assertSuccessful();
+
+        tenancy()->end(); // flush stored tenant instance
+
+        $tenant->putDownForMaintenance();
+
+        $this->expectException(MaintenanceModeException::class);
+        $this->withoutExceptionHandling()
+            ->get('http://acme.localhost/foo');
+    }
+}
+
+class MaintenanceTenant extends Tenant
+{
+    use MaintenanceMode;
+}
