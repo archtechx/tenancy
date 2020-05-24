3cce4f200cca53373df7bf7232cb503db3ce1b5c
stancl <samuel.stancl@gmail.com>
2020-05-24 19:11:01 +0000

    Apply fixes from StyleCI

--- assets/tenant_routes.stub.php
+++ assets/tenant_routes.stub.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
