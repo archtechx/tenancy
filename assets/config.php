616c5983a44fe1c99d9698bdf9b9afd3e425160f
Samuel Štancl <samuel.stancl@gmail.com>
2020-06-05 21:03:55 +0200

    Use default DB connection for central

--- assets/config.php
+++ assets/config.php
@@ -39,7 +39,7 @@ return [
      * Database tenancy config. Used by DatabaseTenancyBootstrapper.
      */
     'database' => [
-        'central_connection' => 'central',
+        'central_connection' => env('DB_CONNECTION', 'central'),
 
         /**
          * Connection used as a "template" for the tenant database connection.
