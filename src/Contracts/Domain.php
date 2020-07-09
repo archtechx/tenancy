4b9b2648d58be2bfb0e5218c82afc827e9356f17
Samuel Štancl <samuel.stancl@gmail.com>
2020-07-09 13:29:04 +0200

    Annotations for Domain contract

--- src/Contracts/Domain.php
+++ src/Contracts/Domain.php
@@ -8,6 +8,10 @@ namespace Stancl\Tenancy\Contracts;
  * @property-read Tenant $tenant
  *
  * @see \Stancl\Tenancy\Database\Models\Domain
+ * 
+ * @method __call(string $method, array $parameters) IDE support. This will be a model.
+ * @method __callStatic(string $method, array $parameters) IDE support. This will be a model.
+ * @mixin \Illuminate\Database\Eloquent\Model
  */
 interface Domain
 {
