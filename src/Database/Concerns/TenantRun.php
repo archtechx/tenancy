8f34a733d80606ca6ff382ac0d45f4d4a37056f6
Samuel Štancl <samuel.stancl@gmail.com>
2020-11-13 08:15:24 +0100

    Add tenancy()->central() helper (#526)
    * Add tenancy()->central() helper
    
    * Apply fixes from StyleCI
    
    * Add docblock
    
    * Fix return typehint
    
    Co-authored-by: stancl <stancl@users.noreply.github.com>

--- src/Database/Concerns/TenantRun.php
+++ src/Database/Concerns/TenantRun.php
@@ -8,6 +8,13 @@ use Stancl\Tenancy\Contracts\Tenant;
 
 trait TenantRun
 {
+    /**
+     * Run a callback in this tenant's context.
+     * Atomic, safely reverts to previous context.
+     *
+     * @param callable $callback
+     * @return mixed
+     */
     public function run(callable $callback)
     {
         /** @var Tenant $this */
--- tests/AutomaticModeTest.php
+++ tests/AutomaticModeTest.php
@@ -71,6 +71,54 @@ class AutomaticModeTest extends TestCase
 
         $this->assertSame('foobar', app('tenancy_initialized_for_tenant'));
     }
+
+    /** @test */
+    public function central_helper_runs_callbacks_in_the_central_state()
+    {
+        tenancy()->initialize($tenant = Tenant::create());
+
+        tenancy()->central(function () {
+            $this->assertSame(null, tenant());
+        });
+
+        $this->assertSame($tenant, tenant());
+    }
+
+    /** @test */
+    public function central_helper_returns_the_value_from_the_callback()
+    {
+        tenancy()->initialize(Tenant::create());
+
+        $this->assertSame('foo', tenancy()->central(function () {
+            return 'foo';
+        }));
+    }
+
+    /** @test */
+    public function central_helper_reverts_back_to_tenant_context()
+    {
+        tenancy()->initialize($tenant = Tenant::create());
+
+        tenancy()->central(function () {
+            //
+        });
+
+        $this->assertSame($tenant, tenant());
+    }
+
+    /** @test */
+    public function central_helper_doesnt_change_tenancy_state_when_called_in_central_context()
+    {
+        $this->assertFalse(tenancy()->initialized);
+        $this->assertNull(tenant());
+
+        tenancy()->central(function () {
+            //
+        });
+
+        $this->assertFalse(tenancy()->initialized);
+        $this->assertNull(tenant());
+    }
 }
 
 class MyBootstrapper implements TenancyBootstrapper
