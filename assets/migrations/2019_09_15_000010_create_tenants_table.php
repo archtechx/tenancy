e596eaaaa67af9a73648446e1ec303fa8b8578e6
Abraham Brookes <abrahambrookes@gmail.com>
2021-03-22 11:30:05 +1000

    Update 2019_09_15_000010_create_tenants_table.php
    just added a comment to clarify how to use these custom columns on the tenant model

--- assets/migrations/2019_09_15_000010_create_tenants_table.php
+++ assets/migrations/2019_09_15_000010_create_tenants_table.php
@@ -18,7 +18,7 @@ class CreateTenantsTable extends Migration
         Schema::create('tenants', function (Blueprint $table) {
             $table->string('id')->primary();
 
-            // your custom columns may go here
+            // your custom columns may go here - remember to add these to your tenant model by using getCustomColumns
 
             $table->timestamps();
             $table->json('data')->nullable();
