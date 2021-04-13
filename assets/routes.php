1b30a806827d1fc619326ced27b8da93047f5c8e
hackerESQ <corey@coreyvarma.com>
2021-04-13 11:46:38 -0500

    Only registers tenant route if enabled in config

--- assets/routes.php
+++ assets/routes.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tenancy/assets/{path?}', 'Stancl\Tenancy\Controllers\TenantAssetsController@asset')
-    ->where('path', '(.*)')
-    ->name('stancl.tenancy.asset');
+if (config('tenancy.serve_tenant_assets', true)) {
+
+    Route::get('/tenancy/assets/{path?}', 'Stancl\Tenancy\Controllers\TenantAssetsController@asset')
+        ->where('path', '(.*)')
+        ->name('stancl.tenancy.asset');
+}
