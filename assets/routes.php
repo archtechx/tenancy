dd005724a3cdea6e9467613cd21d355341970447
hackerESQ <corey@coreyvarma.com>
2021-04-13 11:54:04 -0500

    Uses proper config key

--- assets/routes.php
+++ assets/routes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-if (config('tenancy.serve_tenant_assets', true)) {
+if (config('tenancy.filesystem.serve_tenant_assets', true)) {
 
     Route::get('/tenancy/assets/{path?}', 'Stancl\Tenancy\Controllers\TenantAssetsController@asset')
         ->where('path', '(.*)')
