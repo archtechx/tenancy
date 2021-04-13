b4145374fe729d53813c7fb64bfaa6baa6a9b4f8
hackerESQ <corey@coreyvarma.com>
2021-04-13 12:22:21 -0500

    Move config to service provider

--- assets/routes.php
+++ assets/routes.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-if (config('tenancy.filesystem.serve_tenant_assets', true)) {
-
-    Route::get('/tenancy/assets/{path?}', 'Stancl\Tenancy\Controllers\TenantAssetsController@asset')
-        ->where('path', '(.*)')
-        ->name('stancl.tenancy.asset');
-}
+Route::get('/tenancy/assets/{path?}', 'Stancl\Tenancy\Controllers\TenantAssetsController@asset')
+    ->where('path', '(.*)')
+    ->name('stancl.tenancy.asset');
