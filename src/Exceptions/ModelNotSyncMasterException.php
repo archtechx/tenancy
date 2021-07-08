08eeb9cf8a54b6a728aead20a598abc9d5a74f47
Ralfs Garkaklis <ralfs@garkaklis.com>
2021-07-08 14:24:53 +0300

    Fixed typo

--- src/Exceptions/ModelNotSyncMasterException.php
+++ src/Exceptions/ModelNotSyncMasterException.php
@@ -10,6 +10,6 @@ class ModelNotSyncMasterException extends Exception
 {
     public function __construct(string $class)
     {
-        parent::__construct("Model of $class class is not an SyncMaster model. Make sure you're using the central model to make changes to synced resouces when you're in the central context");
+        parent::__construct("Model of $class class is not an SyncMaster model. Make sure you're using the central model to make changes to synced resources when you're in the central context");
     }
 }
