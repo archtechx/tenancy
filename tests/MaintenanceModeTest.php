7498b1bc1762f415914cafd2d3dd14bf21b1be02
j.stein <joristein@gmail.com>
2021-12-28 14:24:13 +0100

    Add maintenance mode via commands test

--- tests/MaintenanceModeTest.php
+++ tests/MaintenanceModeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Database\Concerns\MaintenanceMode;
 use Stancl\Tenancy\Middleware\CheckTenantForMaintenanceMode;
@@ -48,7 +49,34 @@ class MaintenanceModeTest extends TestCase
     /** @test */
     public function tenant_can_be_in_maintenance_mode_from_command()
     {
+        Route::get('/foo', function () {
+            return 'bar';
+        })->middleware([InitializeTenancyByDomain::class, CheckTenantForMaintenanceMode::class]);
 
+        $tenant = MaintenanceTenant::create();
+        $tenant->domains()->create([
+            'domain' => 'acme.localhost',
+        ]);
+
+        $this->get('http://acme.localhost/foo')
+            ->assertSuccessful();
+
+        tenancy()->end(); // flush stored tenant instance
+
+        Artisan::call('tenancy:down');
+
+        $this->get('http://acme.localhost/foo')
+            ->assertStatus(503);
+
+        tenancy()->end();
+
+        Artisan::call('tenancy:up');
+
+        tenancy()->end();
+
+        $this->get('http://acme.localhost/foo')
+            ->assertSuccessful()
+            ->assertSeeText('bar');
     }
 }
 
