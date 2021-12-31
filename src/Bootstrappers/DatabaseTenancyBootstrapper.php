73a4a3018cadca2ba0fb5f2130fca1718a2b3670
Samuel Štancl <samuel.stancl@gmail.com>
2021-12-31 18:10:03 +0100

    Improve queue tenancy

--- src/Bootstrappers/DatabaseTenancyBootstrapper.php
+++ src/Bootstrappers/DatabaseTenancyBootstrapper.php
@@ -6,6 +6,7 @@ namespace Stancl\Tenancy\Bootstrappers;
 
 use Stancl\Tenancy\Contracts\TenancyBootstrapper;
 use Stancl\Tenancy\Contracts\Tenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\DatabaseManager;
 use Stancl\Tenancy\Exceptions\TenantDatabaseDoesNotExistException;
 
