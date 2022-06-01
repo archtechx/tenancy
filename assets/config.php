fe06c68d04f6f009b05e9734d7ed7ae3f2844eef
Samuel Štancl <samuel@archte.ch>
2022-06-01 15:31:26 +0200

    trigger ci

--- assets/config.php
+++ assets/config.php
@@ -61,7 +61,7 @@ return [
             'sqlite' => Stancl\Tenancy\TenantDatabaseManagers\SQLiteDatabaseManager::class,
             'mysql' => Stancl\Tenancy\TenantDatabaseManagers\MySQLDatabaseManager::class,
             'pgsql' => Stancl\Tenancy\TenantDatabaseManagers\PostgreSQLDatabaseManager::class,
-            'sqlsrv' => Stancl\Tenancy\TenantDatabaseManagers\MicrosoftSQLDatabaseManager::class,
+            'sqlsrv' => Stancl\Tenancy\TenantDatabaseManagers\MicrosoftSQLDatabaseManager::class
 
         /**
          * Use this database manager for MySQL to have a DB user created for each tenant database.
