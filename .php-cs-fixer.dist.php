68846f34d1d1918b79cbe57471064361304f782d
Erik Gaal <me@erikgaal.nl>
2022-06-23 14:50:51 +0200

    use config from archtechx/template

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,107 +1,84 @@
 <?php
 
-$laravel = [
-    'array_indentation' => true,
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$rules = [
     'array_syntax' => ['syntax' => 'short'],
     'binary_operator_spaces' => [
         'default' => 'single_space',
-        'operators' => ['=>' => null],
+        'operators' => [
+            '=>' => null,
+            '|' => 'no_space',
+        ]
     ],
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
+    'no_superfluous_phpdoc_tags' => true,
     'blank_line_before_statement' => [
-        'statements' => ['return'],
+        'statements' => ['return']
     ],
     'braces' => true,
     'cast_spaces' => true,
-    'class_attributes_separation' => [
-        'elements' => [
-            'const' => 'one',
-            'method' => 'one',
-            'property' => 'one',
-            'trait_import' => 'none',
-        ],
-    ],
-    'class_definition' => [
-        'multi_line_extends_each_single_line' => true,
-        'single_item_single_line' => true,
-        'single_line' => true,
-    ],
-    'clean_namespace' => true,
-    'compact_nullable_typehint' => true,
+    'class_definition' => true,
     'concat_space' => [
-        'spacing' => 'none',
+        'spacing' => 'one'
     ],
-    'constant_case' => ['case' => 'lower'],
     'declare_equal_normalize' => true,
     'elseif' => true,
     'encoding' => true,
     'full_opening_tag' => true,
-    'fully_qualified_strict_types' => true,
+    'declare_strict_types' => true,
+    'fully_qualified_strict_types' => true, // added by Shift
     'function_declaration' => true,
     'function_typehint_space' => true,
-    'general_phpdoc_tag_rename' => true,
     'heredoc_to_nowdoc' => true,
     'include' => true,
     'increment_style' => ['style' => 'post'],
     'indentation_type' => true,
-    'integer_literal_case' => true,
-    'lambda_not_used_import' => true,
     'linebreak_after_opening_tag' => true,
     'line_ending' => true,
     'lowercase_cast' => true,
+    'constant_case' => true,
     'lowercase_keywords' => true,
-    'lowercase_static_reference' => true,
-    'magic_method_casing' => true,
+    'lowercase_static_reference' => true, // added from Symfony
+    'magic_method_casing' => true, // added from Symfony
     'magic_constant_casing' => true,
-    'method_argument_space' => [
-        'on_multiline' => 'ignore',
-    ],
-    'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line',
-    ],
+    'method_argument_space' => true,
     'native_function_casing' => true,
-    'native_function_type_declaration_casing' => true,
     'no_alias_functions' => true,
-    'no_alias_language_construct_call' => true,
-    'no_alternative_syntax' => true,
-    'no_binary_string' => true,
-    'no_blank_lines_after_class_opening' => true,
-    'no_blank_lines_after_phpdoc' => true,
-    'no_closing_tag' => true,
-    'no_empty_phpdoc' => true,
-    'no_empty_statement' => true,
     'no_extra_blank_lines' => [
         'tokens' => [
             'extra',
             'throw',
             'use',
-        ],
+            'use_trait',
+        ]
     ],
+    'no_blank_lines_after_class_opening' => true,
+    'no_blank_lines_after_phpdoc' => true,
+    'no_closing_tag' => true,
+    'no_empty_phpdoc' => true,
+    'no_empty_statement' => true,
     'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
     'no_mixed_echo_print' => [
-        'use' => 'echo',
+        'use' => 'echo'
     ],
     'no_multiline_whitespace_around_double_arrow' => true,
+    'multiline_whitespace_before_semicolons' => [
+        'strategy' => 'no_multi_line'
+    ],
     'no_short_bool_cast' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_after_function_name' => true,
-    'no_space_around_double_colon' => true,
-    'no_spaces_around_offset' => [
-        'positions' => ['inside', 'outside'],
-    ],
+    'no_spaces_around_offset' => true,
     'no_spaces_inside_parenthesis' => true,
     'no_trailing_comma_in_list_call' => true,
     'no_trailing_comma_in_singleline_array' => true,
     'no_trailing_whitespace' => true,
     'no_trailing_whitespace_in_comment' => true,
-    'no_unneeded_control_parentheses' => [
-        'statements' => ['break', 'clone', 'continue', 'echo_print', 'return', 'switch_case', 'yield'],
-    ],
-    'no_unneeded_curly_braces' => true,
-    'no_unset_cast' => true,
-    'no_unused_imports' => true,
+    'no_unneeded_control_parentheses' => true,
     'no_unreachable_default_argument_value' => true,
     'no_useless_return' => true,
     'no_whitespace_before_comma_in_array' => true,
@@ -110,33 +87,30 @@ $laravel = [
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'psr_autoloading' => false,
     'phpdoc_indent' => true,
-    'phpdoc_inline_tag_normalizer' => true,
+    'general_phpdoc_tag_rename' => true,
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
     'phpdoc_scalar' => true,
     'phpdoc_single_line_var_spacing' => true,
-    'phpdoc_summary' => false,
+    'phpdoc_summary' => true,
     'phpdoc_to_comment' => false,
-    'phpdoc_tag_type' => true,
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
     'phpdoc_var_without_name' => true,
-    'return_type_declaration' => ['space_before' => 'none'],
+    'psr_autoloading' => true,
     'self_accessor' => true,
     'short_scalar_cast' => true,
-    'simplified_null_return' => false,
+    'simplified_null_return' => false, // disabled by Shift
     'single_blank_line_at_eof' => true,
     'single_blank_line_before_namespace' => true,
-    'single_class_element_per_statement' => [
-        'elements' => ['const', 'property'],
-    ],
+    'single_class_element_per_statement' => true,
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
+    'no_unused_imports' => true,
     'single_line_comment_style' => [
-        'comment_types' => ['hash'],
+        'comment_types' => ['hash']
     ],
     'single_quote' => true,
     'space_after_semicolon' => true,
@@ -144,31 +118,24 @@ $laravel = [
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
     'ternary_operator_spaces' => true,
-    'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+    'trailing_comma_in_multiline' => true,
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
-    'visibility_required' => [
-        'elements' => ['method', 'property'],
-    ],
     'whitespace_after_comma_in_array' => true,
 ];
 
-$rules = array_merge($laravel, [
-    'concat_space' => [
-        'spacing' => 'one',
-    ],
-    'declare_strict_types' => true,
-    'ternary_operator_spaces' => false,
-]);
-
-$finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
+$project_path = getcwd();
+$finder = Finder::create()
+    ->in([
+        $project_path . '/src',
+    ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setFinder($finder)
     ->setRules($rules)
-    ->setRiskyAllowed(true);
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true);
