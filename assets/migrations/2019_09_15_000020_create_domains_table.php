0ad091757a9da30010487c0f25363e2c8a39ecf6
Abrar Ahmad <abrar.dev99@gmail.com>
2022-06-27 16:58:42 +0500

    removed only `onDelete` cascade

--- assets/migrations/2019_09_15_000020_create_domains_table.php
+++ assets/migrations/2019_09_15_000020_create_domains_table.php
@@ -21,6 +21,7 @@ class CreateDomainsTable extends Migration
             $table->string('tenant_id');
 
             $table->timestamps();
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade');
         });
     }
 
