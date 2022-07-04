3e410d2992b1f6f6bf2cb50f25edb432bcedcd9f
Abrar Ahmad <abrar.dev99@gmail.com>
2022-07-04 12:04:54 +0500

    domains set null on delete

--- assets/migrations/2019_09_15_000020_create_domains_table.php
+++ assets/migrations/2019_09_15_000020_create_domains_table.php
@@ -21,7 +21,7 @@ class CreateDomainsTable extends Migration
             $table->string('tenant_id');
 
             $table->timestamps();
-            $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade');
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade')->onDelete('set null');;
         });
     }
 
