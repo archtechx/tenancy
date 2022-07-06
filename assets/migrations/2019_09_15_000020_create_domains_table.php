adce5f34d947f553e657079a7441e28d98c7affe
Abrar Ahmad <abrar.dev99@gmail.com>
2022-07-06 21:50:19 +0500

    revert nullable

--- assets/migrations/2019_09_15_000020_create_domains_table.php
+++ assets/migrations/2019_09_15_000020_create_domains_table.php
@@ -18,7 +18,7 @@ class CreateDomainsTable extends Migration
         Schema::create('domains', function (Blueprint $table) {
             $table->increments('id');
             $table->string('domain', 255)->unique();
-            $table->string('tenant_id')->nullable();
+            $table->string('tenant_id');
 
             $table->timestamps();
             $table->foreign('tenant_id')->references('id')->on('tenants')->onUpdate('cascade');
