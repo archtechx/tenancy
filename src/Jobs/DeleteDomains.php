c0f97fa04ea2c7fb2ab6e353842d5e30dcd545e2
PHP CS Fixer <phpcsfixer@example.com>
2022-07-20 13:29:11 +0000

    Fix code style (php-cs-fixer)

--- src/Jobs/DeleteDomains.php
+++ src/Jobs/DeleteDomains.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
-use Stancl\Tenancy\Database\Models\Domain;
-use Stancl\Tenancy\Events\DatabaseDeleted;
-use Stancl\Tenancy\Events\DeletingDatabase;
-use Stancl\Tenancy\Events\DeletingDomain;
-use Stancl\Tenancy\Events\DomainDeleted;
 
 class DeleteDomains
 {
