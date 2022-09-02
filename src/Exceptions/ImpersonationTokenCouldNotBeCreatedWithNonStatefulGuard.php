7802c280ed75a88af97a647ff6c7fe3ca1e2b62a
lukinovec <lukinovec@gmail.com>
2022-09-02 09:43:34 +0200

    Escape backslashes in the exception's message
    Co-authored-by: Samuel Štancl <samuel.stancl@gmail.com>

--- src/Exceptions/ImpersonationTokenCouldNotBeCreatedWithNonStatefulGuard.php
+++ src/Exceptions/ImpersonationTokenCouldNotBeCreatedWithNonStatefulGuard.php
@@ -10,6 +10,6 @@ class ImpersonationTokenCouldNotBeCreatedWithNonStatefulGuard extends Exception
 {
     public function __construct(string $guardName)
     {
-        parent::__construct("Cannot use a non-stateful auth guard ('$guardName') with user impersonation. Use a guard implementing the Illuminate\Contracts\Auth\StatefulGuard interface.");
+        parent::__construct("Cannot use a non-stateful auth guard ('$guardName') with user impersonation. Use a guard implementing the Illuminate\\Contracts\\Auth\\StatefulGuard interface.");
     }
 }
