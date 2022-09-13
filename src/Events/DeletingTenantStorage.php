1b31bdb56b2860282406b279e17a5df26e8bf104
lukinovec <lukinovec@gmail.com>
2022-09-13 09:22:34 +0200

    Remove tenant storage deletion events

--- src/Events/DeletingTenantStorage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Stancl\Tenancy\Events;
-
-class DeletingTenantStorage extends Contracts\TenantEvent
-{
-}
--- src/Events/TenantStorageDeleted.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Stancl\Tenancy\Events;
-
-class TenantStorageDeleted extends Contracts\TenantEvent
-{
-}
