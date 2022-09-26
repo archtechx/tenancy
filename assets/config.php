b78320b882a1836a704eb5b83ebf6538ef026868
Riley19280 <rileyaven88@gmail.com>
2022-09-26 08:13:58 -0400

    [4.x] Add batch tenancy queue bootstrapper (#874)
    * exclude master from CI
    
    * Add batch tenancy queue bootstrapper
    
    * add test case
    
    * skip tests for old versions
    
    * variable docblocks
    
    * use Laravel's connection getter and setter
    
    * convert test to pest
    
    * bottom space
    
    * singleton regis in TestCase
    
    * Update src/Bootstrappers/BatchTenancyBootstrapper.php
    
    Co-authored-by: Samuel Štancl <samuel@archte.ch>
    
    * convert batch class resolution to property level
    
    * enabled BatchTenancyBootstrapper by default
    
    * typehint DatabaseBatchRepository
    
    * refactore name
    
    * DI DB manager
    
    * typehint
    
    * Update config.php
    
    * use initialize() twice without end()ing tenancy to assert that previousConnection logic works correctly
    
    Co-authored-by: Samuel Štancl <samuel.stancl@gmail.com>
    Co-authored-by: Abrar Ahmad <abrar.dev99@gmail.com>
    Co-authored-by: Samuel Štancl <samuel@archte.ch>

--- assets/config.php
+++ assets/config.php
@@ -32,6 +32,7 @@ return [
         Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\QueueTenancyBootstrapper::class,
+        Stancl\Tenancy\Bootstrappers\BatchTenancyBootstrapper::class,
         // Stancl\Tenancy\Bootstrappers\RedisTenancyBootstrapper::class, // Note: phpredis is needed
     ],
 
