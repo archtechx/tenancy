96a05c483cd2f443b7441144ed561decefc56029
PHP CS Fixer <phpcsfixer@example.com>
2022-09-28 07:49:05 +0000

    Fix code style (php-cs-fixer)

--- src/Commands/Run.php
+++ src/Commands/Run.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Commands;
 
 use Illuminate\Console\Command;
-use Stancl\Tenancy\Concerns\HasTenantOptions;
 use Illuminate\Contracts\Console\Kernel;
+use Stancl\Tenancy\Concerns\HasTenantOptions;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
