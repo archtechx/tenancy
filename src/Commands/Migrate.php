d1a88f57b7b705ed7de6cb0490f4eaef580332a9
lukinovec <lukinovec@gmail.com>
2022-09-29 10:30:44 +0200

    Add missing imports

--- src/Commands/Migrate.php
+++ src/Commands/Migrate.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Commands;
 
 use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Database\Console\Migrations\MigrateCommand;
-use Illuminate\Database\Migrations\Migrator;
-use Stancl\Tenancy\Concerns\ExtendsLaravelCommand;
-use Stancl\Tenancy\Concerns\HasTenantOptions;
 use Stancl\Tenancy\Events\DatabaseMigrated;
+use Illuminate\Database\Migrations\Migrator;
 use Stancl\Tenancy\Events\MigratingDatabase;
+use Stancl\Tenancy\Concerns\HasTenantOptions;
+use Stancl\Tenancy\Concerns\DealsWithMigrations;
+use Stancl\Tenancy\Concerns\ExtendsLaravelCommand;
+use Illuminate\Database\Console\Migrations\MigrateCommand;
 
 class Migrate extends MigrateCommand
 {
