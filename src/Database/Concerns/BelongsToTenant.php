e5bc8ddb776f79f93ee72c0580fb2250e73f7f92
Samuel Štancl <samuel.stancl@gmail.com>
2022-10-01 18:01:39 +0200

    add tenantIdColumn() method (refactor previous commit)

--- src/Database/Concerns/BelongsToTenant.php
+++ src/Database/Concerns/BelongsToTenant.php
@@ -14,7 +14,12 @@ trait BelongsToTenant
 {
     public function tenant()
     {
-        return $this->belongsTo(config('tenancy.tenant_model'), config('tenancy.single_db.tenant_id_column'));
+        return $this->belongsTo(config('tenancy.tenant_model'), static::tenantIdColumn());
+    }
+
+    public static function tenantIdColumn(): string
+    {
+        return config('tenancy.single_db.tenant_id_column');
     }
 
     public static function bootBelongsToTenant(): void
@@ -22,9 +27,9 @@ trait BelongsToTenant
         static::addGlobalScope(new TenantScope);
 
         static::creating(function ($model) {
-            if (! $model->getAttribute(config('tenancy.single_db.tenant_id_column')) && ! $model->relationLoaded('tenant')) {
+            if (! $model->getAttribute(static::tenantIdColumn()) && ! $model->relationLoaded('tenant')) {
                 if (tenancy()->initialized) {
-                    $model->setAttribute(config('tenancy.single_db.tenant_id_column'), tenant()->getTenantKey());
+                    $model->setAttribute(static::tenantIdColumn(), tenant()->getTenantKey());
                     $model->setRelation('tenant', tenant());
                 }
             }
