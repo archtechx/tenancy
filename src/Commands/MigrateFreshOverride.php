3ccf024bac06578c5d971546a2a0ae095f868023
PHP CS Fixer <phpcsfixer@example.com>
2022-10-14 11:49:15 +0000

    Fix code style (php-cs-fixer)

--- src/Commands/MigrateFreshOverride.php
+++ src/Commands/MigrateFreshOverride.php
@@ -11,7 +11,7 @@ class MigrateFreshOverride extends FreshCommand
 {
     public function handle()
     {
-        tenancy()->model()::all()->each(function(TenantWithDatabase $tenant) {
+        tenancy()->model()::all()->each(function (TenantWithDatabase $tenant) {
             if (method_exists($tenant, 'domains')) {
                 $tenant->domains()->delete();
             }
