987ac9a347926e77d36d75039a55423e3de257c3
lukinovec <lukinovec@gmail.com>
2022-10-14 13:48:49 +0200

    Update MigrateFreshOverride

--- src/Commands/MigrateFreshOverride.php
+++ src/Commands/MigrateFreshOverride.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Commands;
 
 use Illuminate\Database\Console\Migrations\FreshCommand;
+use Stancl\Tenancy\Database\Contracts\TenantWithDatabase;
 
 class MigrateFreshOverride extends FreshCommand
 {
     public function handle()
     {
-        dd('overriden');
-        tenancy()->model()::all()->each->delete();
+        tenancy()->model()::all()->each(function(TenantWithDatabase $tenant) {
+            if (method_exists($tenant, 'domains')) {
+                $tenant->domains()->delete();
+            }
+
+            $tenant->delete();
+        });
 
         return parent::handle();
     }
