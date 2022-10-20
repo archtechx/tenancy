34b4f956ab6f6e98ae4e62c2131375b00394aab5
PHP CS Fixer <phpcsfixer@example.com>
2022-10-20 10:31:28 +0000

    Fix code style (php-cs-fixer)

--- src/Jobs/CreatePendingTenants.php
+++ src/Jobs/CreatePendingTenants.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
 use Stancl\Tenancy\Commands\CreatePendingTenants as CreatePendingTenantsCommand;
 
 class CreatePendingTenants implements ShouldQueue
