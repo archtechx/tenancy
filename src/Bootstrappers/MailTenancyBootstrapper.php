54e286d367c205a4cc1b11f01e0546565d6f632b
lukinovec <lukinovec@gmail.com>
2022-10-31 17:39:41 +0100

    Make credentials map a public static property

--- src/Bootstrappers/MailTenancyBootstrapper.php
+++ src/Bootstrappers/MailTenancyBootstrapper.php
@@ -13,13 +13,32 @@ class MailTenancyBootstrapper implements TenancyBootstrapper
 {
     protected array $originalConfig = [];
 
+    /**
+     * Tenant properties to be mapped to config (similarly the TenantConfig feature).
+     *
+     * For example:
+     * [
+     *     'property' => 'config.key.name',
+     * ]
+     */
+    public static array $credentialsMap = [
+        'smtp_transport' => 'mail.mailers.smtp.transport',
+        'smtp_host' => 'mail.mailers.smtp.host',
+        'smtp_port' => 'mail.mailers.smtp.port',
+        'smtp_encryption' => 'mail.mailers.smtp.encryption',
+        'smtp_username' => 'mail.mailers.smtp.username',
+        'smtp_password' => 'mail.mailers.smtp.password',
+        'smtp_timeout' => 'mail.mailers.smtp.timeout',
+        'smtp_local_domain' => 'mail.mailers.smtp.local_domain',
+    ];
+
     public function __construct(protected Repository $config)
     {
     }
 
     public function bootstrap(Tenant $tenant): void
     {
-        foreach ($this->credentialsMap() as $storageKey => $configKey) {
+        foreach (static::$credentialsMap as $storageKey => $configKey) {
             /** @var Tenant&Model $tenant */
             $override = Arr::get($tenant, $storageKey);
 
@@ -44,27 +63,5 @@ class MailTenancyBootstrapper implements TenancyBootstrapper
         foreach ($this->originalConfig as $key => $value) {
             $this->config->set($key, $value);
         }
-    }
-
-    /**
-     * Tenant properties to be mapped to config (similarly the TenantConfig feature).
-     *
-     * For example:
-     * [
-     *     'property' => 'config.key.name',
-     * ]
-     */
-    protected function credentialsMap()
-    {
-        return [
-            'smtp_transport' => 'mail.mailers.smtp.transport',
-            'smtp_host' => 'mail.mailers.smtp.host',
-            'smtp_port' => 'mail.mailers.smtp.port',
-            'smtp_encryption' => 'mail.mailers.smtp.encryption',
-            'smtp_username' => 'mail.mailers.smtp.username',
-            'smtp_password' => 'mail.mailers.smtp.password',
-            'smtp_timeout' => 'mail.mailers.smtp.timeout',
-            'smtp_local_domain' => 'mail.mailers.smtp.local_domain',
-        ];
     }
 }
