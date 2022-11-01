aa536529dfd11fbbca96ba51a937331bc098fea3
Samuel Štancl <samuel.stancl@gmail.com>
2022-11-01 17:48:56 +0100

    update DatabaseSeeder namespace

--- assets/config.php
+++ assets/config.php
@@ -290,7 +290,7 @@ return [
      * Parameters used by the tenants:seed command.
      */
     'seeder_parameters' => [
-        '--class' => 'DatabaseSeeder', // root seeder class
+        '--class' => 'Database\Seeders\DatabaseSeeder', // root seeder class
         // '--force' => true,
     ],
 
