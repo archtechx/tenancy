19deb454eba3f474091a6f14a60871df32c9aa02
lukinovec <lukinovec@gmail.com>
2022-11-02 05:55:14 +0100

    Update comments after review

--- src/TenancyMailManager.php
+++ src/TenancyMailManager.php
@@ -16,11 +16,6 @@ use Stancl\Tenancy\Bootstrappers\MailTenancyBootstrapper;
  * the mailers specified in the static $tenantMailers property
  * instead of getting them from the $mailers property where they're cached.
  *
- * This is mainly used to solve the issue where
- * mail gets sent with the incorrect (old) mailer credentials
- * due to the mailer being cached in the manager's $mailers property
- * and not getting updated when the tenant's credentials change.
- *
  * @see MailTenancyBootstrapper
  */
 class TenancyMailManager extends MailManager
@@ -34,9 +29,8 @@ class TenancyMailManager extends MailManager
     ];
 
     /**
-     * Override the get method so that the mailers in $tenantMailers
-     * always get resolved, even when they're cached and available in the $mailers property
-     * so that the mailers have up-to-date mailer credentials.
+     * Override the get method so that the mailers in $tenantMailers always get resolved,
+     * even when they're cached and available in the $mailers property.
      */
     protected function get($name)
     {
