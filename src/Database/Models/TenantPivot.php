8001cbc0af81bd0cd79ca7e3d3e85685efbb06cc
PHP CS Fixer <phpcsfixer@example.com>
2022-11-08 09:49:33 +0000

    Fix code style (php-cs-fixer)

--- src/Database/Models/TenantPivot.php
+++ src/Database/Models/TenantPivot.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Database\Models;
 
 use Illuminate\Database\Eloquent\Relations\MorphPivot;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 use Stancl\Tenancy\Contracts\Syncable;
 
 class TenantPivot extends MorphPivot
