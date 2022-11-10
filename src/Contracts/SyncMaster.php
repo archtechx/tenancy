dfe1fcecb542e09c874ed74f41013ceb60bdb96b
Abrar Ahmad <abrar.dev99@gmail.com>
2022-11-10 14:36:27 +0500

    use BelongsToMany return type

--- src/Contracts/SyncMaster.php
+++ src/Contracts/SyncMaster.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Contracts;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 // todo move all resource syncing-related things to a separate namespace?
 
@@ -14,7 +14,7 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
  */
 interface SyncMaster extends Syncable
 {
-    public function tenants(): MorphToMany;
+    public function tenants(): BelongsToMany;
 
     public function getTenantModelName(): string;
 }
