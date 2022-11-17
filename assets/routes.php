1db2f0913f00897ee48a07c58962f0060f36d70c
Samuel Štancl <samuel.stancl@gmail.com>
2022-11-17 03:39:57 +0100

    add todo

--- assets/routes.php
+++ assets/routes.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Controllers\TenantAssetController;
 
+// todo make this work with path identification
 Route::get('/tenancy/assets/{path?}', [TenantAssetController::class, 'asset'])
     ->where('path', '(.*)')
     ->name('stancl.tenancy.asset');
