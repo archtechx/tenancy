1e5e60b10d4ee29a6f0a386a6ae462760d1dd25b
Abrar Ahmad <abrar.dev99@gmail.com>
2022-11-23 14:46:37 +0500

    Update ManualModeTest.php

--- tests/ManualModeTest.php
+++ tests/ManualModeTest.php
@@ -3,18 +3,35 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Event;
+use Stancl\JobPipeline\JobPipeline;
+use Stancl\Tenancy\Events\TenancyEnded;
 use Stancl\Tenancy\Events\TenancyInitialized;
+use Stancl\Tenancy\Events\TenantCreated;
+use Stancl\Tenancy\Jobs\CreateDatabase;
 use Stancl\Tenancy\Listeners\CreateTenantConnection;
+use Stancl\Tenancy\Listeners\UseCentralConnection;
+use Stancl\Tenancy\Listeners\UseTenantConnection;
 use \Stancl\Tenancy\Tests\Etc\Tenant;
 
 test('manual tenancy initialization works', function () {
     Event::listen(TenancyInitialized::class, CreateTenantConnection::class);
+    Event::listen(TenancyInitialized::class, UseTenantConnection::class);
+    Event::listen(TenancyEnded::class, UseCentralConnection::class);
 
     $tenant = Tenant::create();
 
-    expect(array_keys(config('database.connections')))->not()->toContain('tenant');
+    expect(array_keys(app('db')->getConnections()))->toBe(['central']);
+    pest()->assertArrayNotHasKey('tenant', config('database.connections'));
 
     tenancy()->initialize($tenant);
-    
-    expect(array_keys(config('database.connections')))->toContain('tenant');
+
+    expect(app('db')->getDefaultConnection())->toBe('tenant');
+    expect(array_keys(app('db')->getConnections()))->toBe(['central', 'tenant']);
+    pest()->assertArrayHasKey('tenant', config('database.connections'));
+
+    tenancy()->end();
+
+    expect(array_keys(app('db')->getConnections()))->toBe(['central']);
+    expect(config('database.connections.tenant'))->toBeNull();
+    expect(app('db')->getDefaultConnection())->toBe(config('tenancy.database.central_connection'));
 });
