f1d454de868874e37de622d036cc968f23e5a9a0
lukinovec <lukinovec@gmail.com>
2022-11-23 13:04:57 +0100

    Use the hardcoded tenant dump path only if the path isn't configured

--- src/Commands/TenantDump.php
+++ src/Commands/TenantDump.php
@@ -23,7 +23,7 @@ class TenantDump extends DumpCommand
     public function handle(ConnectionResolverInterface $connections, Dispatcher $dispatcher): int
     {
         if (is_null($this->option('path'))) {
-            $this->input->setOption('path', database_path('schema/tenant-schema.dump'));
+            $this->input->setOption('path', config('tenancy.migration_parameters.--schema-path') ?? database_path('schema/tenant-schema.dump'));
         }
 
         $tenant = $this->option('tenant')
