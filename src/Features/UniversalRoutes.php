7125e1f3cc673d2b5aa5b39261655e2b294a4432
Abrar Ahmad <abrar.dev99@gmail.com>
2022-11-25 17:36:22 +0500

    Update UniversalRoutes.php

--- src/Features/UniversalRoutes.php
+++ src/Features/UniversalRoutes.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Route as Router;
 use Stancl\Tenancy\Contracts\Feature;
 use Stancl\Tenancy\Middleware;
 
-// todo this got deleted in v4.
 class UniversalRoutes implements Feature
 {
     public static string $middlewareGroup = 'universal';
