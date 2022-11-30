bbb8865b450fcbcea4b413be7b9672f2d5f8dfcc
Abrar Ahmad <abrar.dev99@gmail.com>
2022-11-30 12:36:13 +0500

    function improvements

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,16 +2,16 @@
 
 use Stancl\Tenancy\Tests\TestCase;
 
-uses(TestCase::class)->in(...filesAndFolder());
+uses(TestCase::class)->in(...filesAndFoldersExcluding(['WithoutTenancy']));
 
 function pest(): \Orchestra\Testbench\TestCase
 {
     return Pest\TestSuite::getInstance()->test;
 }
 
-function filesAndFolder(): array
+function filesAndFoldersExcluding(array $exclude = []): array
 {
     $dirs = scandir(__DIR__);
 
-    return array_filter($dirs, fn($dir) => ! in_array($dir, ['.', '..', 'WithoutTenancy'], true));
+    return array_filter($dirs, fn($dir) => ! in_array($dir, array_merge(['.', '..'], $exclude) , true));
 }
