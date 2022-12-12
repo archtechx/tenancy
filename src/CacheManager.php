d3c7fef002ab2ae94a55e13f21903af252389933
lukinovec <lukinovec@gmail.com>
2022-12-12 17:16:53 +0100

    Revert condition in CacheManager to avoid excessive nesting

--- src/CacheManager.php
+++ src/CacheManager.php
@@ -19,26 +19,26 @@ class CacheManager extends BaseCacheManager
     public function __call($method, $parameters)
     {
         // todo0 Make PrefixCacheTenancyBootstrapper work with tags
-        if (tenancy()->initialized) {
-            $tags = [config('tenancy.cache.tag_base') . tenant()?->getTenantKey()];
-
-            if ($method === 'tags') {
-                $count = count($parameters);
-
-                if ($count !== 1) {
-                    throw new \Exception("Method tags() takes exactly 1 argument. $count passed.");
-                }
-
-                $names = $parameters[0];
-                $names = (array) $names; // cache()->tags('foo') https://laravel.com/docs/9.x/cache#removing-tagged-cache-items
-
-                return $this->store()->tags(array_merge($tags, $names));
-            }
-
-            return $this->store()->tags($tags)->$method(...$parameters);
+        if (! tenancy()->initialized) {
+            return parent::__call($method, $parameters);
         }
 
-        return parent::__call($method, $parameters);
+        $tags = [config('tenancy.cache.tag_base') . tenant()?->getTenantKey()];
+
+        if ($method === 'tags') {
+            $count = count($parameters);
+
+            if ($count !== 1) {
+                throw new \Exception("Method tags() takes exactly 1 argument. $count passed.");
+            }
+
+            $names = $parameters[0];
+            $names = (array) $names; // cache()->tags('foo') https://laravel.com/docs/9.x/cache#removing-tagged-cache-items
+
+            return $this->store()->tags(array_merge($tags, $names));
+        }
+
+        return $this->store()->tags($tags)->$method(...$parameters);
     }
 
     public function refreshStore(string|null $repository = null): void
