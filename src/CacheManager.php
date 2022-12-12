bc70a2fab89e93c19e042e1ad0d8476d90a771e2
PHP CS Fixer <phpcsfixer@example.com>
2022-12-12 16:25:57 +0000

    Fix code style (php-cs-fixer)

--- src/CacheManager.php
+++ src/CacheManager.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Cache\CacheManager as BaseCacheManager;
+use Illuminate\Support\Facades\Cache;
 
 // todo move to Cache namespace?
 
