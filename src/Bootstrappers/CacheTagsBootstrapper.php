ea805fa23108f1bc763b4eaf0bbc057090734e09
PHP CS Fixer <phpcsfixer@example.com>
2023-04-20 13:49:48 +0000

    Fix code style (php-cs-fixer)

--- src/Bootstrappers/CacheTagsBootstrapper.php
+++ src/Bootstrappers/CacheTagsBootstrapper.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Bootstrappers;
 
 use Stancl\Tenancy\CacheManager;
-use Stancl\Tenancy\Contracts\Tenant;
 use Stancl\Tenancy\Contracts\TenancyBootstrapper;
+use Stancl\Tenancy\Contracts\Tenant;
 
 /**
  * todo name.
