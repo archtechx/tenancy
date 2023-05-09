3c098dc78ea95465358b741b7cd81c7be47fdb5f
lukinovec <lukinovec@gmail.com>
2023-05-09 15:13:10 +0200

    Add Postgres bootstrapper

---
+++ src/Bootstrappers/Integrations/PostgresTenancyBootstrapper.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stancl\Tenancy\Bootstrappers\Integrations;
+
+use Stancl\Tenancy\Contracts\Tenant;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Contracts\Config\Repository;
+use Stancl\Tenancy\Contracts\TenancyBootstrapper;
+
+class PostgresTenancyBootstrapper implements TenancyBootstrapper
+{
+    protected array $originalCentralConnectionConfig;
+    protected array $originalPostgresConfig;
+
+    public function __construct(
+        protected Repository $config,
+        protected DatabaseManager $database,
+    ) {
+        $this->originalCentralConnectionConfig = $config->get('database.connections.central');
+        $this->originalPostgresConfig = $config->get('database.connections.pgsql');
+    }
+
+    public function bootstrap(Tenant $tenant): void
+    {
+        $this->database->purge('central');
+
+        $this->config->set('database.connections.pgsql.username', $tenant->getTenantKey());
+        $this->config->set('database.connections.pgsql.password', $tenant->database()->getPassword() ?? 'password');
+        $this->config->set('database.connections.central', $this->config->get('database.connections.pgsql'));
+    }
+
+    public function revert(): void
+    {
+        $this->database->purge('central');
+
+        $this->config->set('database.connections.central', $this->originalCentralConnectionConfig);
+        $this->config->set('database.connections.pgsql', $this->originalPostgresConfig);
+    }
+}
