43d167bb48d30c75571b96adbc0d2d096e221092
lukinovec <lukinovec@gmail.com>
2023-06-16 10:35:44 +0200

    Comment out dropping policies

--- src/Commands/CreateRLSPoliciesForTenantTables.php
+++ src/Commands/CreateRLSPoliciesForTenantTables.php
@@ -34,7 +34,7 @@ class CreateRLSPoliciesForTenantTables extends Command
         $table = $model->getTable();
         $tenantKey = tenancy()->tenantKeyColumn();
 
-        DB::statement("DROP POLICY IF EXISTS {$table}_rls_policy ON {$table}");
+        // DB::statement("DROP POLICY IF EXISTS {$table}_rls_policy ON {$table}");
 
         if (tenancy()->modelBelongsToTenant($model)) {
             DB::statement("CREATE POLICY {$table}_rls_policy ON {$table} USING ({$tenantKey}::TEXT = current_user);");
