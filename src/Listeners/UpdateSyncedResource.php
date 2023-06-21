a56c70779fb25a31c47234b8c79e75570c002a72
Ion Caliman <icaliman92@gmail.com>
2023-06-21 18:59:35 +0300

    Fix: Sync when ID's are duplicated

--- src/Listeners/UpdateSyncedResource.php
+++ src/Listeners/UpdateSyncedResource.php
@@ -60,7 +60,10 @@ class UpdateSyncedResource extends QueueableListener
             } else {
                 // If the resource doesn't exist at all in the central DB,we create
                 // the record with all attributes, not just the synced ones.
-                $centralModel = $event->model->getCentralModelName()::create($event->model->getAttributes());
+                $attributes = $event->model->getAttributes();
+                unset($attributes['id']);
+
+                $centralModel = $event->model->getCentralModelName()::create($attributes);
                 event(new SyncedResourceChangedInForeignDatabase($event->model, null));
             }
         });
