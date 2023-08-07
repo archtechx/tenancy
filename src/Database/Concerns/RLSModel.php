90dc8d50beb16bc4d729d8f2053cb9b43da8edcb
lukinovec <lukinovec@gmail.com>
2023-08-07 10:54:23 +0200

    Add RLSModel

---
+++ src/Database/Concerns/RLSModel.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stancl\Tenancy\Database\Concerns;
+
+/**
+ * Interface indicating that the queries of the model it's used on
+ * get scoped using RLS instead of the global TenantScope.
+ *
+ * Used with Postgres RLS (single-database tenancy).
+ *
+ * @see \Stancl\Tenancy\Database\Concerns\BelongsToTenant
+ */
+interface RLSModel
+{
+}
