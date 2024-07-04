590360bf0d583fa1a98961c27d7d7a36501aff8c
Samuel Štancl <samuel@archte.ch>
2024-07-04 20:52:06 +0200

    broadcasting config bootstrapper: add reverb preset

--- src/Bootstrappers/BroadcastingConfigBootstrapper.php
+++ src/Bootstrappers/BroadcastingConfigBootstrapper.php
@@ -37,6 +37,12 @@ class BroadcastingConfigBootstrapper implements TenancyBootstrapper
             'broadcasting.connections.pusher.app_id' => 'pusher_app_id',
             'broadcasting.connections.pusher.options.cluster' => 'pusher_cluster',
         ],
+        'reverb' => [
+            'broadcasting.connections.reverb.key' => 'reverb_key',
+            'broadcasting.connections.reverb.secret' => 'reverb_secret',
+            'broadcasting.connections.reverb.app_id' => 'reverb_app_id',
+            'broadcasting.connections.reverb.options.cluster' => 'reverb_cluster',
+        ],
         'ably' => [
             'broadcasting.connections.ably.key' => 'ably_key',
             'broadcasting.connections.ably.public' => 'ably_public',
