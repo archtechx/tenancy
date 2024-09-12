35f724128b9b74c15d9547054631ccc12d7a8081
Chinmay Purav <chinpurav@gmail.com>
2024-09-12 23:37:23 +0530

    events dispatched

--- src/Database/Concerns/MaintenanceMode.php
+++ src/Database/Concerns/MaintenanceMode.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace Stancl\Tenancy\Database\Concerns;
 
 use Carbon\Carbon;
+use Stancl\Tenancy\Events\TenantGoingInMaintenanceMode;
+use Stancl\Tenancy\Events\TenantWentInMaintenanceMode;
 
 trait MaintenanceMode
 {
     public function putDownForMaintenance($data = [])
     {
+        event(new TenantGoingInMaintenanceMode($this));
+
         $this->update(['maintenance_mode' => [
             'time' => $data['time'] ?? Carbon::now()->getTimestamp(),
             'message' => $data['message'] ?? null,
             'retry' => $data['retry'] ?? null,
             'allowed' => $data['allowed'] ?? [],
         ]]);
+
+        event(new TenantWentInMaintenanceMode($this));
     }
 }
