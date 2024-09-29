075789eb91cd173d8ddddccdaa2010229a6ef0db
Samuel Štancl <samuel@archte.ch>
2024-09-29 12:25:46 +0200

    fix typo in ParallelCommand

--- src/Concerns/ParallelCommand.php
+++ src/Concerns/ParallelCommand.php
@@ -61,7 +61,7 @@ trait ParallelCommand
 
         // perflevel0 refers to P-cores on M-series, and the entire CPU on Intel Macs
         if ($darwin && $ffi->sysctlbyname('hw.perflevel0.logicalcpu', FFI::addr($cores), FFI::addr($size), null, 0) === 0) {
-            return $size->cdata;
+            return $cores->cdata;
         } else if ($darwin) {
             // Reset the size in case the pointer got written to (likely shouldn't happen)
             $size->cdata = FFI::sizeof($cores);
