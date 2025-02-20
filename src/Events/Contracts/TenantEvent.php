b14f13a8961a15869517dc864fb63a774d0b87a2
Samuel Štancl <samuel@archte.ch>
2025-02-20 17:35:26 +0100

    remove JobPipeline todo

--- src/Events/Contracts/TenantEvent.php
+++ src/Events/Contracts/TenantEvent.php
@@ -7,7 +7,7 @@ namespace Stancl\Tenancy\Events\Contracts;
 use Illuminate\Queue\SerializesModels;
 use Stancl\Tenancy\Contracts\Tenant;
 
-abstract class TenantEvent // todo0 we could add a feature to JobPipeline that automatically gets data for the send() from here
+abstract class TenantEvent
 {
     use SerializesModels;
 
