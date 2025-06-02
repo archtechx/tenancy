e74e1f92e12726b34739d7d2057a1a459a881927
Samuel Štancl <samuel@archte.ch>
2025-06-02 20:34:49 +0200

    Make RouteMode enum backed (#1362)

--- src/Enums/RouteMode.php
+++ src/Enums/RouteMode.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Stancl\Tenancy\Enums;
 
-enum RouteMode
+/**
+ * Note: The backing values are not part of the public API and are subject to change.
+ */
+enum RouteMode: int
 {
-    case TENANT;
-    case CENTRAL;
-    case UNIVERSAL;
+    case CENTRAL   = 0b01;
+    case TENANT    = 0b10;
+    case UNIVERSAL = 0b11;
 }
