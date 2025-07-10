90272bcbb196c539a9a7993151d04dce3f6d8b98
Leandro G. Gehlen <leandrogehlen@gmail.com>
2025-07-10 07:15:32 -0300

    Fix central model creation to use only synced attributes

--- src/Listeners/UpdateSyncedResource.php
+++ src/Listeners/UpdateSyncedResource.php
@@ -60,7 +60,9 @@ class UpdateSyncedResource extends QueueableListener
             } else {
                 // If the resource doesn't exist at all in the central DB,we create
                 // the record with all attributes, not just the synced ones.
-                $centralModel = $event->model->getCentralModelName()::create($event->model->getAttributes());
+
+                // It's necessary to use $syncedAttributes when tenant model contains extra attributes to central model
+                $centralModel = $event->model->getCentralModelName()::create($syncedAttributes);
                 event(new SyncedResourceChangedInForeignDatabase($event->model, null));
             }
         });
