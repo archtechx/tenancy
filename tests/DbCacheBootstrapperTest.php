d482bb9e2d4b4a96e5826d37cd4f410e2a921c69
lukinovec <lukinovec@gmail.com>
2025-07-22 19:55:04 +0200

    Simplify low-level DatabaseCacheBootstrapper test, delete migration file

--- tests/DbCacheBootstrapperTest.php
+++ tests/DbCacheBootstrapperTest.php
@@ -14,15 +14,22 @@ use Stancl\Tenancy\Events\TenancyInitialized;
 use Stancl\Tenancy\Listeners\BootstrapTenancy;
 use Stancl\Tenancy\Events\TenancyEnded;
 use Stancl\Tenancy\Listeners\RevertToCentralContext;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 beforeEach(function () {
-    Event::listen(TenantCreated::class, JobPipeline::make([CreateDatabase::class])->send(function (TenantCreated $event) {
-        return $event->tenant;
-    })->toListener());
+    Event::listen(
+        TenantCreated::class,
+        JobPipeline::make([CreateDatabase::class])->send(function (TenantCreated $event) {
+            return $event->tenant;
+        })->toListener()
+    );
 
     Event::listen(TenancyInitialized::class, BootstrapTenancy::class);
     Event::listen(TenancyEnded::class, RevertToCentralContext::class);
+});
 
+test('DatabaseCacheBootstrapper makes cache use the tenant connection', function() {
     config([
         'cache.default' => 'database',
         'tenancy.bootstrappers' => [
@@ -30,24 +37,35 @@ beforeEach(function () {
             DatabaseCacheBootstrapper::class, // Used instead of CacheTenancyBootstrapper
         ],
     ]);
-});
 
-test('DatabaseCacheBootstrapper uses tenant connection for saving cache', function() {
-    pest()->artisan('migrate', [
-        '--path' => __DIR__ . '/Etc/cache_migrations',
-        '--realpath' => true,
-    ])->assertExitCode(0);
-
+    // DB query for verifying that the cache is stored in DB
+    // under the 'foo' key (prefixed by the default cache prefix).
     $databaseCacheKey = config('cache.prefix') . 'foo';
     $databaseCacheValue = fn () => DB::selectOne("SELECT * FROM `cache` WHERE `key` = '{$databaseCacheKey}'")?->value;
+
+    $createCacheTables = function () {
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
+    };
+
+    // Create cache tables in central DB
+    $createCacheTables();
 
     $tenant = Tenant::create();
     $tenant2 = Tenant::create();
 
-    pest()->artisan('tenants:migrate', [
-        '--path' => __DIR__ . '/Etc/cache_migrations',
-        '--realpath' => true,
-    ])->assertExitCode(0);
+    // Create cache tables in tenant DBs
+    // With this bootstrapper, cache will be saved in these tenant DBs instead of the central DB
+    tenancy()->runForMultiple([$tenant, $tenant2], $createCacheTables);
 
     // Write to cache in central context
     cache(['foo' => 'CENTRAL']);
@@ -60,18 +78,15 @@ test('DatabaseCacheBootstrapper uses tenant connection for saving cache', functi
     tenancy()->end();
 
     // The 'foo' cache value in the central database should be 'CENTRAL'
-    expect(cache('foo'))->toBe('CENTRAL');
     expect(str($databaseCacheValue())->contains('CENTRAL'))->toBeTrue();
 
     tenancy()->initialize($tenant);
 
     // The 'foo' cache value in the tenant database should be 'TENANT'
-    expect(cache('foo'))->toBe('TENANT');
     expect(str($databaseCacheValue())->contains('TENANT'))->toBeTrue();
 
     tenancy()->initialize($tenant2);
 
-    // The 'foo' cache value in another tenant's database should be null
-    expect(cache('foo'))->toBeNull();
+    // The 'foo' cache key in another tenant's database shouldn't exist
     expect(DB::select('select * from cache'))->toHaveCount(0);
 });
