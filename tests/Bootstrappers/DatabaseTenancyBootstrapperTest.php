59ab773aa8aed13c4c9f25de040fcc74ab3e5075
lukinovec <lukinovec@gmail.com>
2026-06-30 07:12:12 +0200

    Use `$centralConnection` variable in both hardening tests for consistency
    Decided to go with the variable assignment instead of inlining the config(...) call in the assertion since it's a bit more readable imo

--- tests/Bootstrappers/DatabaseTenancyBootstrapperTest.php
+++ tests/Bootstrappers/DatabaseTenancyBootstrapperTest.php
@@ -115,7 +115,9 @@ test('harden prevents tenants from using the database of another tenant', functi
         expect(fn () => tenancy()->initialize($tenant))->toThrow(RuntimeException::class);
 
         // Connection should be reverted back to central
-        expect(DB::connection()->getName())->toBe(config('tenancy.database.central_connection'));
+        $centralConnection = config('tenancy.database.central_connection');
+
+        expect(DB::connection()->getName())->toBe($centralConnection);
     } else {
         expect(fn() => tenancy()->initialize($tenant))->not()->toThrow(Throwable::class);
 
