8e671fe5bef82690eadc2336253e74d2420ec2bd
lukinovec <lukinovec@gmail.com>
2026-08-31 16:28:12 +0200

    Use "placed within" instead of "appended to" in log bootstrapper comment
    In response to https://github.com/archtechx/tenancy/pull/1479#discussion_r3876808350

--- src/Bootstrappers/LogChannelBootstrapper.php
+++ src/Bootstrappers/LogChannelBootstrapper.php
@@ -155,7 +155,7 @@ class LogChannelBootstrapper implements TenancyBootstrapper
 
                 // The tenant log will inherit the segment that follows the storage path from the central channel path config.
                 // For example, if a channel's path is configured to storage_path('logs/foo.log') (storage/logs/foo.log),
-                // the '/logs/foo.log' segment will be appended to the tenant storage path (so the log will be located at storage/tenant123/logs/foo.log).
+                // the '/logs/foo.log' segment will be placed within the tenant storage path (so the log will be located at storage/tenant123/logs/foo.log).
                 $this->config->set("logging.channels.{$channel}.path", $tenantStoragePath . Str::after($originalChannelPath, $centralStoragePath));
             }
         }
