9ec732cfce239f661bcf080530f6cff5dbc19c97
lukinovec <lukinovec@gmail.com>
2026-09-11 07:05:10 +0200

    rtrim directory and public root in remove symlinks action
    realpath() already returns paths without the trailing separator -- rtrim is used just so that the code is more self=documenting.

--- src/Actions/RemoveStorageSymlinksAction.php
+++ src/Actions/RemoveStorageSymlinksAction.php
@@ -66,7 +66,7 @@ class RemoveStorageSymlinksAction
 
         // Remove the directories CreateStorageSymlinksAction created for the symlink
         // until a non-empty one is reached.
-        while (str_starts_with($directory, $publicRoot . DIRECTORY_SEPARATOR) && $files->isEmptyDirectory($directory)) {
+        while (str_starts_with(rtrim($directory, '/\\'), rtrim($publicRoot, '/\\') . DIRECTORY_SEPARATOR) && $files->isEmptyDirectory($directory)) {
             $files->deleteDirectory($directory);
 
             $directory = dirname($directory);
